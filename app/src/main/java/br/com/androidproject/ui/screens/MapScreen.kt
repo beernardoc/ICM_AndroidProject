f5c5aba858cc1bb417fea3ae3c957fbc343c2289
beernardoc <bernardopinto@ua.pt>
2024-06-19 00:24:08 +0100

    merge

--- app/src/main/java/br/com/androidproject/ui/screens/MapScreen.kt
+++ app/src/main/java/br/com/androidproject/ui/screens/MapScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
